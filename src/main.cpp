d72948820bd98b5788738dd41f44d46fe2e42eb9
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-08-16 18:02:38 +0900

    Arduino keyboard mouse

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <Arduino.h>
 
+#include <M5Unified.h>
+
+#include "USB.h"
+#include "USBHIDKeyboard.h"
+#include "USBHIDMouse.h"
+
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <WiFiClientSecure.h>
@@ -21,9 +27,13 @@ extern const uint8_t _binary_data_assets_index_js_gz_end[] asm("_binary_data_ass
 extern const uint8_t _binary_data_assets_style_css_gz_start[] asm("_binary_data_assets_style_css_gz_start");
 extern const uint8_t _binary_data_assets_style_css_gz_end[] asm("_binary_data_assets_style_css_gz_end");
 
-#define CH9329_TX_PIN GPIO_NUM_6
-#define CH9329_RX_PIN GPIO_NUM_7
-#define LED_PIN GPIO_NUM_2
+USBHID HID;
+USBHIDKeyboard Keyboard;
+USBHIDMouse Mouse;
+
+#define ENABLE_USB_DEVICE 1
+
+#define LED_PIN GPIO_NUM_35
 
 #define LED_COLOR_SETUP Adafruit_NeoPixel::Color(0, 32, 0)
 #define LED_COLOR_NORMAL Adafruit_NeoPixel::Color(0, 0, 32)
@@ -35,21 +45,26 @@ WebSocketsServer webSocket = WebSocketsServer(81);
 WebServer server(80);
 Adafruit_NeoPixel pixels(1, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+uint8_t prevKeys[6] = {0, 0, 0, 0, 0, 0};
+uint8_t prevModifier = 0;
+uint8_t prevMouseKey = 0;
+
 void hexdump(const void *mem, uint32_t len, uint8_t cols = 16)
 {
   const uint8_t *src = (const uint8_t *)mem;
-  Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+  Serial.printf("[HEXDUMP] Address: 0x%08X len: 0x%X (%d)\r\n", (ptrdiff_t)src, len, len);
   for (uint32_t i = 0; i < len; i++)
   {
     if (i % cols == 0)
     {
-      Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+      Serial.printf("[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
     }
     Serial.printf("%02X ", *src);
     src++;
   }
-  Serial.printf("\n");
+  Serial.printf("\r\n");
 }
+
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 {
@@ -92,7 +107,116 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 
     if (payload[0] == 0x57)
     {
-      Serial1.write(payload, length);
+      if (payload[3] == 0x02)
+      {
+        // キーボード入力
+
+        for (int i = 0; i < 6; i++)
+        {
+          if (payload[7 + i] == 0)
+            continue;
+
+          for (int j = 0; j < 6; j++)
+          {
+            if (prevKeys[j] == payload[7 + i])
+              continue;
+          }
+#ifdef ENABLE_DEBUG_PRINT
+          Serial.printf("Key Pressed: %02X\r\n", payload[7 + i]);
+#endif
+#ifdef ENABLE_USB_DEVICE
+          Keyboard.press(payload[7 + i]);
+#endif
+        }
+        for (int i = 0; i < 6; i++)
+        {
+          if (prevKeys[i] == 0)
+            continue;
+          for (int j = 0; j < 6; j++)
+          {
+            if (prevKeys[i] == payload[7 + j])
+              continue;
+          }
+#ifdef ENABLE_DEBUG_PRINT
+          Serial.printf("Key Released: %02X\r\n", prevKeys[i]);
+#endif
+#ifdef ENABLE_USB_DEVICE
+          Keyboard.release(prevKeys[i]);
+#endif
+        }
+        for (int i = 0; i < 6; i++)
+        {
+          prevKeys[i] = payload[7 + i];
+        }
+
+        uint8_t modifier = payload[5];
+
+        for (int i = 0; i < 8; i++)
+        {
+          uint8_t b = 1 << i;
+          if (prevModifier & b == modifier & b)
+            continue;
+          if (modifier & b)
+          {
+#ifdef ENABLE_DEBUG_PRINT
+            Serial.printf("Key Pressed: %02X\r\n", 0x80 + i);
+#endif
+#ifdef ENABLE_USB_DEVICE
+            Keyboard.press(0x80 + i);
+#endif
+          }
+          if (prevModifier & b)
+          {
+#ifdef ENABLE_DEBUG_PRINT
+            Serial.printf("Key Released: %02X\r\n", 0x80 + i);
+#endif
+#ifdef ENABLE_USB_DEVICE
+            Keyboard.release(0x80 + i);
+#endif
+          }
+        }
+        prevModifier = modifier;
+      }
+
+      if (payload[3] == 0x05)
+      {
+        int8_t x = payload[7];
+        int8_t y = payload[8];
+        int8_t w = payload[9];
+#ifdef ENABLE_DEBUG_PRINT
+        Serial.printf("Mouse x=%02d,y=%02d,w=%02d\r\n", x, y, w);
+#endif
+#ifdef ENABLE_USB_DEVICE
+        Mouse.move(x, y, w);
+#endif
+
+        uint8_t mouseKey = payload[6];
+        for (int i = 0; i < 3; i++)
+        {
+          uint8_t b = 1 << i;
+
+          if(b & prevMouseKey == b & mouseKey)
+            continue;
+
+          if(b & mouseKey){
+#ifdef ENABLE_DEBUG_PRINT
+            Serial.printf("Mouse press=%d\r\n", b);
+#endif
+#ifdef ENABLE_USB_DEVICE
+          Mouse.press(b);
+#endif
+          }
+          if(b & prevMouseKey){
+#ifdef ENABLE_DEBUG_PRINT
+            Serial.printf("Mouse release=%d\r\n", b);
+#endif
+#ifdef ENABLE_USB_DEVICE
+          Mouse.release(b);
+#endif
+          }
+        }
+        prevMouseKey = payload[6];
+      }
     }
 
     break;
@@ -120,6 +244,15 @@ static void sendStatic(const char *mime,
 
 void setup()
 {
+  M5.begin();
+
+#ifdef ENABLE_USB_DEVICE
+  HID.begin();
+  USB.begin();
+  Keyboard.begin();
+  Mouse.begin();
+#endif
+
 #ifdef ENABLE_DEBUG_PRINT
   Serial.begin(115200);
 
@@ -134,9 +267,6 @@ void setup()
 
   pixels.setPixelColor(0, LED_COLOR_SETUP);
   pixels.show();
-
-  Serial1.setPins(CH9329_RX_PIN, CH9329_TX_PIN);
-  Serial1.begin(9600);
 
   delay(1000);
 
@@ -176,6 +306,15 @@ void setup()
 
 void loop()
 {
+  M5.update();
   webSocket.loop();
   server.handleClient();
+  if (M5.BtnA.wasClicked())
+  {
+#ifdef ENABLE_DEBUG_PRINT
+    Serial.print("Connected! IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.flush();
+#endif
+  }
 }
